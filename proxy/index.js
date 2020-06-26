adee12bad4a31f6c1d83a07a364043505f5180ff
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-26 08:26:59 +0000

    Hack our custom proxy server to keep alive websocket via ping packet

--- proxy/index.js
+++ proxy/index.js
@@ -21,5 +21,24 @@ server.on('upgrade', function (req, socket, head) {
     proxy.ws(req, socket, head);
 });
 
+const t = new Uint8Array([0x89, 0x82, 0x01, 0x01, 0x01, 0x01, 0x01 ^ 0x68, 0x01 ^ 0x69]);
+
+proxy.on('open', function(soc) {
+    console.log('open evt');
+    setInterval(function(s) {
+        try {
+            if (!s.writable) {
+                console.log('not writable');
+                clearInterval(this);
+            } else {
+                s.write(t);
+            }
+        } catch (err) {
+            console.log(err);
+            clearInterval(this);
+        }
+    }, 5 * 1000, soc);
+})
+
 console.log("listening on port 5050")
 server.listen(5050);
