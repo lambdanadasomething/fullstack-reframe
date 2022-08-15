945267d4c5c27be06ddde94a32c2dbc3514e52c2
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2022-08-15 02:40:20 +0000

    [minor] start journey towards SSR

--- crudy/src/cljs/crudy/server.cljs
+++ crudy/src/cljs/crudy/server.cljs
@@ -1,16 +1,32 @@
 (ns crudy.server
-  (:require ["express" :as express]))
+  (:require ["express" :as express]
+            ["fs" :as fs]
+            [macchiato.server :as http]
+            [taoensso.timbre :as timbre
+             :refer-macros [log  trace  debug  info  warn  error  fatal  report
+                            logf tracef debugf infof warnf errorf fatalf reportf
+                            spy get-env]]))
 
 ;; currently broken in shadow-cljs
 (set! *warn-on-infer* true)
 
 (defonce server (atom nil))
 
+(defn handler
+  [request callback]
+  (callback {:status 200
+             :body "Hello Macchiato"}))
+
 (defn start-server []
-  (println "Starting server")
-  (let [app (express)]
-    (.get app "/" (fn [req res] (.send res "Hello, world tesla")))
-    (.listen app 3000 (fn [] (println "Example app listening on port 3000!")))))
+  (info "Hey I am running now!")
+  (let [host "0.0.0.0"
+        port 8290]
+    (http/start
+     {:handler    handler
+      :host       host
+      :port       port
+      :on-success #(info "macchiato-test started on" host ":" port)})))
+
 
 (defn start! []
   ;; called by main and after reloading code
